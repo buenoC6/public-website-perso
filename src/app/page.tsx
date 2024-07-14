b1ad4280a84e4c17c67276bb9d96794542d276fb
Alexian Moins <alexian.moins@outlook.com>
2024-07-14 18:25:58 +0200

    fix ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import { Section } from '@/components/Section'
 import { Card } from '@chakra-ui/card'
 import { Select } from '@chakra-ui/select'
+import { Stack } from '@chakra-ui/react'
 
 export default function Home() {
   return (
@@ -13,28 +14,42 @@ export default function Home() {
             mesure
           </p>
           <h2>Web, Desktop & Mobile</h2>
-          <Card padding={7} gap={5} width={'100%'} maxWidth={'600px'}>
-            <Select cursor={'pointer'} placeholder="Votre type d'application">
-              <option>Application web / SaaS</option>
-              <option>Application mobile</option>
-              <option>Site e-commerce</option>
-              <option>Marketplace</option>
-              <option>Site vitrine</option>
-              <option>Autre</option>
-            </Select>
-            <Select cursor={'pointer'} placeholder="Votre budget">
-              <option>- de 5000€</option>
-              <option>5000 - 10 000€</option>
-              <option>20 000 - 50 000€</option>
-              <option>+ de 50 000€</option>
-              <option>Autre</option>
-            </Select>
-            <Select cursor={'pointer'} placeholder="Votre délais de livraison">
-              <option>- de 1 mois</option>
-              <option>1 à 3 mois</option>
-              <option>3 à 6 mois</option>
-              <option>+ de 1 an</option>
-            </Select>
+          <Card
+            variant={'elevated'}
+            borderRadius={20}
+            borderTop={'8px solid #FFD780'}
+            shadow={'lg'}
+            padding={7}
+            gap={5}
+            width={'100%'}
+            maxWidth={'600px'}
+          >
+            <Stack spacing={5} pt={3}>
+              <Select cursor={'pointer'} placeholder="Votre type d'application">
+                <option>Application web / SaaS</option>
+                <option>Application mobile</option>
+                <option>Site e-commerce</option>
+                <option>Marketplace</option>
+                <option>Site vitrine</option>
+                <option>Autre</option>
+              </Select>
+              <Select cursor={'pointer'} placeholder="Votre budget">
+                <option>- de 5000€</option>
+                <option>5000 - 10 000€</option>
+                <option>20 000 - 50 000€</option>
+                <option>+ de 50 000€</option>
+                <option>Autre</option>
+              </Select>
+              <Select
+                cursor={'pointer'}
+                placeholder="Votre délais de livraison"
+              >
+                <option>- de 1 mois</option>
+                <option>1 à 3 mois</option>
+                <option>3 à 6 mois</option>
+                <option>+ de 1 an</option>
+              </Select>
+            </Stack>
             <button>Démarrez votre projet</button>
           </Card>
         </Section>
@@ -42,13 +57,20 @@ export default function Home() {
           <div className={'link-card-container'}>
             <ul>
               <li>
-                <h2>Nos services</h2>
-                <p>
-                  Création graphique, mise en place de votre site web ou devotre
-                  application mobile. Augmentation de votre présence dans les
-                  résultats des moteurs de recherche.
-                </p>
-                <button>Démarrez votre projet</button>
+                <Card
+                  variant={'elevated'}
+                  borderRadius={20}
+                  shadow={'lg'}
+                  padding={7}
+                >
+                  <h2>Nos services</h2>
+                  <p>
+                    Création graphique, mise en place de votre site web ou
+                    devotre application mobile. Augmentation de votre présence
+                    dans les résultats des moteurs de recherche.
+                  </p>
+                  <button>Démarrez votre projet</button>
+                </Card>
               </li>
               <li>
                 <h3>Site web</h3>
@@ -58,44 +80,36 @@ export default function Home() {
                 </p>
               </li>
               <li>
-                <div>
-                  <h3>Application mobile</h3>
-                  <p>
-                    {
-                      "Création d'application mobile sur mesure, application mobile hybride, application mobile native, etc."
-                    }
-                  </p>
-                </div>
+                <h3>Application mobile</h3>
+                <p>
+                  {
+                    "Création d'application mobile sur mesure, application mobile hybride, application mobile native, etc."
+                  }
+                </p>
               </li>
               <li>
-                <div>
-                  <h3>Référencement</h3>
-                  <p>
-                    Augmentation et/ou amélioration de votre présence dans les
-                    résultats des moteurs de recherche, optimisation de votre
-                    site web.
-                  </p>
-                </div>
+                <h3>Référencement</h3>
+                <p>
+                  Augmentation et/ou amélioration de votre présence dans les
+                  résultats des moteurs de recherche, optimisation de votre site
+                  web.
+                </p>
               </li>
               <li>
-                <div>
-                  <h3>Marketing digital</h3>
-                  <p>
-                    {
-                      'Création de campagnes publicitaires, gestion de vos réseaux sociaux, etc.'
-                    }
-                  </p>
-                </div>
+                <h3>Marketing digital</h3>
+                <p>
+                  {
+                    'Création de campagnes publicitaires, gestion de vos réseaux sociaux, etc.'
+                  }
+                </p>
               </li>
               <li>
-                <div>
-                  <h3>Formation</h3>
-                  <p>
-                    {
-                      "Formation à la création de site web, à l'utilisation de votre application mobile, etc."
-                    }
-                  </p>
-                </div>
+                <h3>Formation</h3>
+                <p>
+                  {
+                    "Formation à la création de site web, à l'utilisation de votre application mobile, etc."
+                  }
+                </p>
               </li>
             </ul>
           </div>
