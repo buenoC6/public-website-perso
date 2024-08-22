2d122a1c81f761b59a8236b84b013ab83b1da871
Alexian Moins <47835444+buenoC6@users.noreply.github.com>
2024-08-22 11:34:13 +0200

    Update layout.tsx

--- src/app/[language]/layout.tsx
+++ src/app/[language]/layout.tsx
@@ -1,6 +1,7 @@
 import React, { cache } from 'react'
 import { getStrapiClient } from '@/services/Strapi'
 import { notFound } from 'next/navigation'
+import Script from 'next/script'
 
 const getPage = cache(async function getPage(locale: string, url: string) {
   const client = getStrapiClient()
@@ -21,5 +22,28 @@ export default async function LanguageLayout({
 
   if (!pages.data || pages.data.length < 1) notFound()
 
-  return <>{children}</>
+  return <>
+    <Script id="axeption-cookies">
+      {`window.axeptioSettings = {
+        clientId: "66c7043d437ae8dd9f5b9ea4",
+        cookiesVersion: "netbuddies-fr-EU",
+        googleConsentMode: {
+          default: {
+            analytics_storage: "denied",
+            ad_storage: "denied",
+            ad_user_data: "denied",
+            ad_personalization: "denied",
+            wait_for_update: 500
+          }
+        }
+      };
+       
+      (function(d, s) {
+        var t = d.getElementsByTagName(s)[0], e = d.createElement(s);
+        e.async = true; e.src = "//static.axept.io/sdk.js";
+        t.parentNode.insertBefore(e, t);
+      })(document, "script");`}
+    </Script>
+    {children}
+  </>
 }
