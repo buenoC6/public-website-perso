6539e619baf3e6be81c387ec5078fa6f01e28e3d
Alexian Moins <alexian.moins@outlook.com>
2024-09-06 20:54:48 +0200

    fix footer & ui

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,37 +1,57 @@
+import Link from 'next/link'
+
 export default function Footer() {
   return (
-    <footer style={{ display: 'grid' }}>
+    <footer>
       <SvgWaves />
       <div
         style={{
-          color: 'white !important',
-          padding: '20px',
-          margin: '0px',
           backgroundColor: '#004e98',
+          padding: '50px 20px 20px 20px',
+          margin: '0px',
         }}
       >
-        <ul
+        <div
           style={{
-            width: '100%',
             display: 'flex',
-            listStyle: 'none',
-            gap: '25px',
-            flexWrap: 'wrap',
+            justifyContent: 'center',
           }}
         >
-          <li style={{ flex: '1 1 250px', textAlign: 'center' }}>
-            Conditions générales
-          </li>
-          <li style={{ flex: '1 1 250px', textAlign: 'center' }}>
-            Politique de confidentialité
-          </li>
-          <li style={{ flex: '1 1 250px', textAlign: 'center' }}>
-            ByteBuddies
-          </li>
-        </ul>
-        <hr style={{ margin: '30px 0px' }} />
-        <div style={{ textAlign: 'center', margin: '0px' }}>
-          <span>© 2021 ByteBuddies</span>
+          <ul
+            style={{
+              color: 'white',
+              width: '100%',
+              maxWidth: '1200px',
+              display: 'flex',
+              listStyle: 'none',
+              gap: '25px',
+              flexWrap: 'wrap',
+            }}
+          >
+            <Link
+              href={'/conditions-generales-de-vente'}
+              style={{ flex: '1 1 250px', textAlign: 'center' }}
+            >
+              Conditions générales
+            </Link>
+            <li style={{ flex: '1 1 250px', textAlign: 'center' }}>
+              Politique de confidentialité
+            </li>
+            <li style={{ flex: '1 1 250px', textAlign: 'center' }}>
+              <Link href={'mailto:contact@netbuddies.be'}>
+                contact@netbuddies.be
+              </Link>
+            </li>
+            <li style={{ flex: '1 1 250px', textAlign: 'center' }}>
+              <div>+32 485 73 80 56</div>
+            </li>
+          </ul>
+        </div>
+        <hr style={{ margin: '60px 0px 40px 0px' }} />
+        <div
+          style={{ textAlign: 'center', margin: '20px 0px', color: 'white' }}
+        >
+          <span>© 2024 NetBuddies</span>
         </div>
       </div>
     </footer>
@@ -41,7 +61,11 @@ export default function Footer() {
 const SvgWaves = () => (
   <svg
     id="wave"
-    style={{ transform: 'rotate(0deg)', transition: '0.3' }}
+    style={{
+      transform: 'rotate(0deg)',
+      transition: '0.3',
+      marginBottom: '-12px',
+    }}
     viewBox="0 0 1440 170"
     version="1.1"
     xmlns="http://www.w3.org/2000/svg"
