f8b18f652784ae2c8e4530691b65ba34ba9c360d
Alexian Moins <alexian.moins@outlook.com>
2024-09-06 20:59:58 +0200

    title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="msapplication-TileColor" content="##007bff" />
         <meta name="theme-color" content="#ffffff" />
+        <title>NetBuddies</title>
       </head>
       <body>
         <Header />
