0d3e5b7042a9e1810f844f11449edd0fb0c2f1dc
Alexian Moins <alexian.moins@outlook.com>
2024-09-10 22:49:17 +0200

    headers

--- next.config.mjs
+++ next.config.mjs
@@ -9,19 +9,19 @@ const nextConfig = {
             headers:[{
                 key:"content-security-policy",
                 value:"" +
-                    "default-src 'self' https://api.axept.io https://netbuddies.be ;" +
+                    "default-src 'self' https://api.axept.io https://netbuddies.be;" +
                     " style-src 'self' 'unsafe-inline' fonts.googleapis.com;" +
                     " base-uri 'none';" +
                     " form-action 'self';" +
-                    " script-src 'self' https://matomo.alexianmoins.be https://client.axept.io;" +
+                    " script-src 'self' https://matomo.alexianmoins.be https://client.axept.io https://static.axept.io;" +
                     " font-src 'self' fonts.gstatic.com;" +
                     " manifest-src 'self';" +
                     " img-src 'self' blob: " +
                     " https://strapi.netbuddies.be " +
                     " data:" +
                     " https://ik.imagekit.io" +
-                    " connect-src 'self' https://matomo.alexianmoins.be https://api.axept.io https://netbuddies.be https://client.axept.io; " +
-                    " script-src-elem 'self' 'unsafe-inline' 'unsafe-eval' https://static.axept.io/sdk.js https://netbuddies.be ;"
+                    " connect-src 'self' https://matomo.alexianmoins.be https://api.axept.io https://netbuddies.be https://client.axept.io;" +
+                    " script-src-elem 'self' 'unsafe-inline' 'unsafe-eval' https://static.axept.io https://netbuddies.be;"
             }]
         }]
     },
