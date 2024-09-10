6641f68b7a542552fad1dd89d9917a2440572eb1
Alexian Moins <alexian.moins@outlook.com>
2024-09-10 22:52:39 +0200

    headers

--- next.config.mjs
+++ next.config.mjs
@@ -6,23 +6,22 @@ const nextConfig = {
     async headers(){
         return [{
             source:"/:all*",
-            headers:[{
-                key:"content-security-policy",
-                value:"" +
-                    "default-src 'self' https://api.axept.io https://netbuddies.be;" +
-                    " style-src 'self' 'unsafe-inline' fonts.googleapis.com;" +
-                    " base-uri 'none';" +
-                    " form-action 'self';" +
-                    " script-src 'self' https://matomo.alexianmoins.be https://client.axept.io https://static.axept.io;" +
-                    " font-src 'self' fonts.gstatic.com;" +
-                    " manifest-src 'self';" +
-                    " img-src 'self' blob: " +
-                    " https://strapi.netbuddies.be " +
-                    " data:" +
-                    " https://ik.imagekit.io" +
-                    " connect-src 'self' https://matomo.alexianmoins.be https://api.axept.io https://netbuddies.be https://client.axept.io;" +
-                    " script-src-elem 'self' 'unsafe-inline' 'unsafe-eval' https://static.axept.io https://netbuddies.be;"
-            }]
+            headers: [
+                {
+                    key: "Content-Security-Policy",
+                    value: `
+                        default-src 'self' https://api.axept.io https://netbuddies.be;
+                        style-src 'self' 'unsafe-inline' fonts.googleapis.com;
+                        base-uri 'none';
+                        form-action 'self';
+                        script-src 'self' https://matomo.alexianmoins.be https://client.axept.io https://static.axept.io 'unsafe-inline' 'unsafe-eval';
+                        font-src 'self' fonts.gstatic.com;
+                        manifest-src 'self';
+                        img-src 'self' blob: data: https://strapi.netbuddies.be https://ik.imagekit.io;
+                        connect-src 'self' https://matomo.alexianmoins.be https://api.axept.io https://netbuddies.be https://client.axept.io;
+                    `.replace(/\s{2,}/g, ' ').trim()
+                }
+            ]
         }]
     },
     async redirects() {
