66634ef4a2f14347e5866a07ccf2bf3515c91354
Alexian Moins <alexian.moins@outlook.com>
2024-09-10 22:33:32 +0200

    headers

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,27 @@ const nextConfig = {
     distDir: 'dist',
     output: 'export',
     staticPageGenerationTimeout: 300,
+    async headers(){
+        return [{
+            source:"/:all*",
+            headers:[{
+                key:"content-security-policy",
+                value:"" +
+                    "default-src 'self' data: ;" +
+                    " style-src 'self' 'unsafe-inline' fonts.googleapis.com;" +
+                    " base-uri 'none';" +
+                    " form-action 'self';" +
+                    " script-src 'self' 'unsafe-eval' 'unsafe-inline';" +
+                    " font-src 'self' fonts.gstatic.com;" +
+                    " manifest-src 'self';" +
+                    " img-src 'self' blob: " +
+                    " https://strapi.netbuddies.be" +
+                    " data:" +
+                    " https://ik.imagekit.io" +
+                    " connect-src 'self' https://matomo.alexianmoins.be;"
+            }]
+        }]
+    },
     async redirects() {
         return [
             {
